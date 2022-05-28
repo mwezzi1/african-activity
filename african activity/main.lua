b78cba2344af730bda127d003fb62fee13866940
mwezzi1 <104887029+mwezzi1@users.noreply.github.com>
2022-05-27 23:37:24 -0700

    Update main.lua

--- african activity/main.lua
+++ african activity/main.lua
@@ -1,7 +1,15 @@
-function love.start()
-
+function love.load()
+   player = {}
+   player.x = 500
+   player.y = 500
+   
+   player.sprite = love.graphics.newImage('sprites/Phillip Ray Lester.png')
 end
 
 function love.update()
-    
+
+end
+
+function love.draw()
+    love.graphics.draw(player.sprite, player.x, player.y)
 end
