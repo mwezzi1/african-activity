6df56c8014e46d12c73af68a09b72fcc3480828f
mwezzi1 <104887029+mwezzi1@users.noreply.github.com>
2022-05-28 20:32:08 -0700

    Add files via upload

--- african activity/main.lua
+++ african activity/main.lua
@@ -1,15 +1,39 @@
 function love.load()
+   local tween = require 'tween'
+
    player = {}
+   player.sprite = love.graphics.newImage('sprites/Phillip Ray Lester.png')
    player.x = 500
    player.y = 500
    
-   player.sprite = love.graphics.newImage('sprites/Phillip Ray Lester.png')
+   speed = 1 
+
+   bat = {}
+   bat.sprite = love.graphics.newImage('sprites/bat.png')
+   toggled = false
 end
 
-function love.update()
-
-end
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + speed
+    end
+  
+    if love.keyboard.isDown("left") then
+         player.x = player.x - speed
+    end
+    
+    if love.keyboard.isDown("down") then
+      player.y = player.y + speed
+    end
+    
+    if love.keyboard.isDown("up") then
+        player.y = player.y - speed
+    end
+  end
 
 function love.draw()
     love.graphics.draw(player.sprite, player.x, player.y)
+    if toggled == true then
+        love.graphics.draw(bat.sprite)
+    end
 end
